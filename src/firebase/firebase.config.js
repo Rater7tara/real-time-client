fdced154a2467c1a920ca87bcf6eb89a245d4aa8
Rater7tara <nowshinakteremu005@gmail.com>
2024-02-02 00:29:51 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBWnLzHld0C1nuVVsWENTbS6t5xp4rFFAA",
+  authDomain: "real-time-ebb08.firebaseapp.com",
+  projectId: "real-time-ebb08",
+  storageBucket: "real-time-ebb08.appspot.com",
+  messagingSenderId: "520616021250",
+  appId: "1:520616021250:web:63e72c0d851830206f061b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
